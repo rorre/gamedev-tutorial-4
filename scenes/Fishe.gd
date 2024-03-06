e6b38da299185081f54abff55b1d1170e2dc1f0a
Ren <renrror@gmail.com>
2024-03-06 20:34:19 +0700

    a

--- scenes/Fishe.gd
+++ scenes/Fishe.gd
@@ -20,5 +20,3 @@ export var sceneName = "LoseScreen"
 func _on_Fishe_body_entered(body: Node) -> void:
 	if body.get_name() == "Player":
 		get_tree().change_scene(str("res://scenes/" + sceneName + ".tscn"))
-	else:
-		body.queue_free()
